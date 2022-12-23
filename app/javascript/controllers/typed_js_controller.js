c0c179ee774c952d3b8b5efe4cff01ec7bdb369c
root <karine.egler@gmail.com>
2022-12-23 07:50:31 +0100

    Ajout card-product

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -6,7 +6,7 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["S'informer", "Prendre connaissance"],
+      strings: ["S'informer sur le Monde Informatique", "Prendre connaissance des innovations technologiques"],
       typeSpeed: 50,
       loop: true
     })
